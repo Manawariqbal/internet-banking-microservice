b5a1f7a5269dd01b58d8f2020781075eee8d4c8a
Md Manawar Iqbal <manawariqbal@Mohmmeds-MacBook-Pro.local>
2025-12-04 15:07:40 +0530

    Initial internet banking gateway is created

---
+++ internet-banking-api-gateway/src/main/java/com/manawar/finance/InternetBankingApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.manawar.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class InternetBankingApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetBankingApiGatewayApplication.class, args);
+	}
+
+}
